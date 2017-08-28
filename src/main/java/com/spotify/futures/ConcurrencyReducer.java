738a16ace8eafb1e51a4ddff8fd8bb896af6af5e
Evan Shrubsole <eshr@spotify.com>
2017-08-28 12:01:59 +0200

    Copy new grabJob implementation from futures-extra#28

--- src/main/java/com/spotify/futures/ConcurrencyReducer.java
+++ src/main/java/com/spotify/futures/ConcurrencyReducer.java
@@ -116,18 +116,16 @@ public class ConcurrencyReducer<T> {
    * and does not acquire a permit from the semaphore
    */
   private Job<T> grabJob() {
-    while (!queue.isEmpty()) {
-      if (!limit.tryAcquire()) {
-        return null;
-      }
-
-      final Job<T> job = queue.poll();
-      if (job != null) {
-        return job;
-      }
-
-      limit.release();
+    if (!limit.tryAcquire()) {
+      return null;
     }
+
+    final Job<T> job = queue.poll();
+    if (job != null) {
+      return job;
+    }
+
+    limit.release();
     return null;
   }
 
