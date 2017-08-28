d617a83c68c07935e995100a5e6678e4bd03b454
Evan Shrubsole <evanshrubsole@gmail.com>
2017-08-28 14:43:41 +0200

    Port ConcurrecnyLimiter from FuturesExtra to work with CompletionStage (#53)
    * Port ConcurrecnyLimiter from FuturesExtra to work with CompletionStage
    
    This is a pretty straightforward port, and no extra optimizations were
    made.
    
    * Rename to ConcurrencyLimiter to avoid name conflicts with FuturesExtra
    
    * Copy new grabJob implementation from futures-extra#28

---
+++ src/main/java/com/spotify/futures/ConcurrencyReducer.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2017 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.spotify.futures;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Semaphore;
+
+/**
+ * {@link ConcurrencyReducer} is used to queue tasks which will be
+ * executed in a manner reducing the number of concurrent tasks.
+ *
+ * Note: This is a port of ConcurrencyLimiter from futures-extra for use with CompletionStages
+ */
+public class ConcurrencyReducer<T> {
+
+  private final BlockingQueue<Job<T>> queue;
+  private final Semaphore limit;
+  private final int maxQueueSize;
+
+  private final int maxConcurrency;
+
+  private ConcurrencyReducer(int maxConcurrency, int maxQueueSize) {
+    this.maxConcurrency = maxConcurrency;
+    this.maxQueueSize = maxQueueSize;
+    if (maxConcurrency <= 0) {
+      throw new IllegalArgumentException("maxConcurrency must be at least 0");
+    }
+
+    if (maxQueueSize <= 0) {
+      throw new IllegalArgumentException("maxQueueSize must be at least 0");
+    }
+    this.queue = new ArrayBlockingQueue<>(maxQueueSize);
+    this.limit = new Semaphore(maxConcurrency);
+  }
+
+  /**
+   * @param maxConcurrency maximum number of futures in progress,
+   * @param maxQueueSize   maximum number of jobs in queue. This is a soft bound and may be
+   *                       temporarily exceeded if add() is called concurrently.
+   * @return a new concurrency limiter
+   */
+  public static <T> ConcurrencyReducer<T> create(int maxConcurrency, int maxQueueSize) {
+    return new ConcurrencyReducer<>(maxConcurrency, maxQueueSize);
+  }
+
+  /**
+   * the callable function will run as soon as the currently active set of futures is less than the
+   * maxConcurrency limit.
+   *
+   * @param callable - a function that creates a future.
+   * @return a proxy future that completes with the future created by the input function. This
+   * future will be immediately failed with {@link CapacityReachedException} if the soft queue
+   * size limit is exceeded.
+   */
+  public CompletableFuture<T> add(final Callable<? extends CompletionStage<T>> callable) {
+    requireNonNull(callable);
+    final CompletableFuture<T> response = new CompletableFuture<>();
+    final Job<T> job = new Job<>(callable, response);
+    if (!queue.offer(job)) {
+      final String message = "Queue size has reached capacity: " + maxQueueSize;
+      return CompletableFutures.exceptionallyCompletedFuture(new CapacityReachedException(message));
+    }
+    pump();
+    return response;
+  }
+
+  /**
+   * @return the number of callables that are queued up and haven't started yet.
+   */
+  public int numQueued() {
+    return queue.size();
+  }
+
+  /**
+   * @return the number of currently active futures that have not yet completed.
+   */
+  public int numActive() {
+    return maxConcurrency - limit.availablePermits();
+  }
+
+  /**
+   * @return the number of additional callables that can be queued before failing.
+   */
+  public int remainingQueueCapacity() {
+    return queue.remainingCapacity();
+  }
+
+  /**
+   * @return the number of additional callables that can be run without queueing.
+   */
+  public int remainingActiveCapacity() {
+    return limit.availablePermits();
+  }
+
+  /**
+   * Does one of two things: 1) return a job and acquire a permit from the semaphore 2) return null
+   * and does not acquire a permit from the semaphore
+   */
+  private Job<T> grabJob() {
+    if (!limit.tryAcquire()) {
+      return null;
+    }
+
+    final Job<T> job = queue.poll();
+    if (job != null) {
+      return job;
+    }
+
+    limit.release();
+    return null;
+  }
+
+  private void pump() {
+    Job<T> job;
+    while ((job = grabJob()) != null) {
+      final CompletableFuture<T> response = job.response;
+
+      if (response.isCancelled()) {
+        limit.release();
+      } else {
+        invoke(response, job.callable);
+      }
+    }
+  }
+
+  private void invoke(final CompletableFuture<T> response,
+                      final Callable<? extends CompletionStage<T>> callable) {
+    final CompletionStage<T> future;
+    try {
+      future = callable.call();
+      if (future == null) {
+        limit.release();
+        response.completeExceptionally(new NullPointerException());
+        return;
+      }
+    } catch (Throwable e) {
+      limit.release();
+      response.completeExceptionally(e);
+      return;
+    }
+
+    future.whenComplete(
+        (result, t) -> {
+          if (result != null) {
+            limit.release();
+            response.complete(result);
+            pump();
+          } else {
+            limit.release();
+            response.completeExceptionally(t);
+            pump();
+          }
+        });
+  }
+
+  private static class Job<T> {
+
+    private final Callable<? extends CompletionStage<T>> callable;
+    private final CompletableFuture<T> response;
+
+    public Job(Callable<? extends CompletionStage<T>> callable, CompletableFuture<T> response) {
+      this.callable = callable;
+      this.response = response;
+    }
+  }
+
+  public static class CapacityReachedException extends RuntimeException {
+
+    public CapacityReachedException(String errorMessage) {
+      super(errorMessage);
+    }
+  }
+}
---
+++ src/test/java/com/spotify/futures/ConcurrencyReducerTest.java
@@ -0,0 +1,260 @@
+/*
+ * Copyright (c) 2017 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.spotify.futures;
+
+import static com.spotify.futures.CompletableFutures.getException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+import org.junit.Test;
+
+public class ConcurrencyReducerTest {
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testTooLowConcurrency() throws Exception {
+    ConcurrencyReducer.create(0, 10);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testTooLowQueueSize() throws Exception {
+    ConcurrencyReducer.create(10, 0);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullJob() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(1, 10);
+    limiter.add(null);
+  }
+
+  @Test
+  public void testJobReturnsNull() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(1, 10);
+    final CompletableFuture<String> response = limiter.add(job(null));
+    assertTrue(response.isDone());
+    final Throwable exception = getException(response);
+    assertThat(exception, instanceOf(NullPointerException.class));
+  }
+
+  @Test
+  public void testJobThrows() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(1, 10);
+    final CompletableFuture<String> response =
+        limiter.add(() -> {
+              throw new IllegalStateException();
+            });
+
+    assertTrue(response.isDone());
+    final Throwable exception = getException(response);
+    assertThat(exception, instanceOf(IllegalStateException.class));
+  }
+
+  @Test
+  public void testJobReturnsFailure() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(1, 10);
+    final CompletionStage<String> response =
+        limiter.add(job(CompletableFutures.exceptionallyCompletedFuture(new IllegalStateException())));
+
+    assertTrue(response.toCompletableFuture().isDone());
+    final Throwable exception = getException(response);
+    assertThat(exception, instanceOf(IllegalStateException.class));
+  }
+
+  @Test
+  public void testCancellation() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(2, 10);
+    final CompletableFuture<String> request1 = new CompletableFuture<>();
+    final CompletableFuture<String> request2 = new CompletableFuture<>();
+
+    final CompletableFuture<String> response1 = limiter.add(job(request1));
+    final CompletableFuture<String> response2 = limiter.add(job(request2));
+
+    final AtomicBoolean wasInvoked = new AtomicBoolean();
+    final CompletableFuture<String> response3 =
+        limiter.add(
+            () -> {
+              wasInvoked.set(true);
+              return null;
+            });
+
+    response3.toCompletableFuture().cancel(false);
+
+    // 1 and 2 are in progress, 3 is cancelled
+
+    assertFalse(response1.isDone());
+    assertFalse(response2.isDone());
+    assertTrue(response3.isDone());
+    assertEquals(2, limiter.numActive());
+    assertEquals(1, limiter.numQueued());
+
+    request2.complete("2");
+
+    assertFalse(response1.isDone());
+    assertTrue(response2.isDone());
+    assertTrue(response3.isDone());
+    assertEquals(1, limiter.numActive());
+    assertEquals(0, limiter.numQueued());
+
+    request1.complete("1");
+
+    assertTrue(response1.isDone());
+    assertTrue(response2.isDone());
+    assertTrue(response3.isDone());
+    assertEquals(0, limiter.numActive());
+    assertEquals(0, limiter.numQueued());
+
+    assertFalse(wasInvoked.get());
+  }
+
+  @Test
+  public void testSimple() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(2, 10);
+    final CompletableFuture<String> request1 = new CompletableFuture<>();
+    final CompletableFuture<String> request2 = new CompletableFuture<>();
+    final CompletableFuture<String> request3 = new CompletableFuture<>();
+    final CompletableFuture<String> response1 = limiter.add(job(request1));
+    final CompletableFuture<String> response2 = limiter.add(job(request2));
+    final CompletableFuture<String> response3 = limiter.add(job(request3));
+
+    request3.complete("3");
+
+    // 1 and 2 are in progress, 3 is still blocked
+
+    assertFalse(response1.isDone());
+    assertFalse(response2.isDone());
+    assertFalse(response3.isDone());
+    assertEquals(2, limiter.numActive());
+    assertEquals(1, limiter.numQueued());
+
+    request2.complete("2");
+
+    assertFalse(response1.isDone());
+    assertTrue(response2.isDone());
+    assertTrue(response3.isDone());
+    assertEquals(1, limiter.numActive());
+    assertEquals(0, limiter.numQueued());
+
+    request1.complete("1");
+
+    assertTrue(response1.isDone());
+    assertTrue(response2.isDone());
+    assertTrue(response3.isDone());
+    assertEquals(0, limiter.numActive());
+    assertEquals(0, limiter.numQueued());
+  }
+
+  @Test
+  public void testLongRunning() throws Exception {
+    final AtomicInteger activeCount = new AtomicInteger();
+    final AtomicInteger maxCount = new AtomicInteger();
+    final int queueSize = 11;
+    final int maxConcurrency = 10;
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(maxConcurrency, queueSize);
+    List<CountingJob> jobs = new ArrayList<>();
+    List<CompletableFuture<String>> responses = new ArrayList<>();
+    for (int i = 0; i < queueSize; i++) {
+      final CountingJob job = new CountingJob(limiter::numActive, maxCount);
+      jobs.add(job);
+      responses.add(limiter.add(job));
+    }
+
+    for (int i = 0; i < jobs.size(); i++) {
+      final CountingJob job = jobs.get(i);
+      if (i % 2 == 0) {
+        job.future.complete("success");
+      } else {
+        job.future.completeExceptionally(new IllegalStateException());
+      }
+    }
+    responses.forEach(response -> assertTrue(response.isDone()));
+    assertEquals(0, activeCount.get());
+    assertEquals(0, limiter.numActive());
+    assertEquals(0, limiter.numQueued());
+    assertEquals(maxConcurrency, limiter.remainingActiveCapacity());
+    assertEquals(maxConcurrency, maxCount.get());
+    assertEquals(queueSize, limiter.remainingQueueCapacity());
+  }
+
+  @Test
+  public void testQueueSize() throws Exception {
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(10, 10);
+    for (int i = 0; i < 20; i++) {
+      limiter.add(job(new CompletableFuture<>()));
+    }
+
+    final CompletableFuture<String> future = limiter.add(job(new CompletableFuture<>()));
+    assertTrue(future.isDone());
+    final Throwable e = getException(future);
+    assertThat(e, instanceOf(ConcurrencyReducer.CapacityReachedException.class));
+  }
+
+  @Test
+  public void testQueueSizeCounter() throws Exception {
+    final CompletableFuture<String> future = new CompletableFuture<>();
+
+    final ConcurrencyReducer<String> limiter = ConcurrencyReducer.create(10, 10);
+    for (int i = 0; i < 20; i++) {
+      limiter.add(job(future));
+    }
+
+    assertEquals(10, limiter.numActive());
+    assertEquals(10, limiter.numQueued());
+    assertEquals(0, limiter.remainingActiveCapacity());
+    assertEquals(0, limiter.remainingQueueCapacity());
+
+    future.complete("");
+
+    assertEquals(0, limiter.numActive());
+    assertEquals(0, limiter.numQueued());
+    assertEquals(10, limiter.remainingActiveCapacity());
+    assertEquals(10, limiter.remainingQueueCapacity());
+  }
+
+  private Callable<CompletionStage<String>> job(final CompletionStage<String> future) {
+    return () -> future;
+  }
+
+  private static class CountingJob implements Callable<CompletionStage<String>> {
+
+    private final Supplier<Integer> activeCount;
+    private final AtomicInteger maxCount;
+
+    final CompletableFuture<String> future = new CompletableFuture<>();
+
+    public CountingJob(Supplier<Integer> activeCount, AtomicInteger maxCount) {
+      this.activeCount = activeCount;
+      this.maxCount = maxCount;
+    }
+
+    @Override
+    public CompletionStage<String> call() throws Exception {
+      if (activeCount.get() > maxCount.get()) {
+        maxCount.set(activeCount.get());
+      }
+      return future;
+    }
+  }
+}
