c467f71cfba9faa5b93012eb675b1ddd055e5b0d
Michael Scibor <mscibor@spotify.com>
2017-10-02 15:04:32 -0400

    Added unit test

--- src/test/java/com/spotify/futures/FunctionsTest.java
+++ src/test/java/com/spotify/futures/FunctionsTest.java
@@ -66,4 +66,10 @@ public class FunctionsTest {
     f.andThen(null);
   }
 
+  @Test
+  public void function6_andThenNull() {
+    final Function6<String, String, String, String, String, String, Integer> ff = (a, b, c, d, e, f) -> 1;
+    exception.expect(NullPointerException.class);
+    ff.andThen(null);
+  }
 }
